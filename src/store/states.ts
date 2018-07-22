c82e17c6b3afed0bb1ae67b40ffc1c8bc2813e8c
yufangyang <975057282@qq.com>
2018-07-22 21:22:38 +0800

    ajax

--- src/store/states.ts
+++ src/store/states.ts
@@ -93,6 +93,9 @@ const state: State = {
         source: {
           xAxis: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
           data: [820, 932, 901, 934, 1290, 1330, 1320]
+        },
+        ajax: {
+          url: ''
         }
       },
       extra: {
